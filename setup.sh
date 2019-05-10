8a113642cc830844691b9c6af4afb2bd393aeab8
Jakob Sautner <lappalal@users.noreply.github.com>
2019-05-10 06:43:17 +0200

    brew bundle

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 
 # Install the contents of "./Brewfile"
 curl -O "https://raw.githubusercontent.com/lappalal/macos-setup/master/Brewfile" -o ~/.Brewfile
-brew bundle --global --verboose
+brew bundle --file=~/Brewfile --verboose
 
 # Install Oh-My-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
